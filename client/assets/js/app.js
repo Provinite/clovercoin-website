fef322586ecf91f2a7caae826ec47ba20b166ce3
prov <provinite@gmail.com>
2016-07-18 00:33:12 -0500

    Updates links in footer to absolute urls. Hides contact form. Fixes broken link in footer. Replaces JS-based links with appropriate a tags.

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -1,12 +1,1 @@
 $(document).foundation();
-$('.full-spread-menu > div').each(function() {
-    $(this).click(function() {
-        if ($(this).data('href')) {
-            if ($(this).data('target') == '_blank') {
-                window.open($(this).data('href'));
-            } else {
-                window.top.location.href = $(this).data('href');
-            }
-        }
-    });
-});
